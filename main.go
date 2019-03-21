a8c332c9e031af33deb85e564de52198b414745f
Luis Fernando Gaido <lfgaido@correios.com.br>
2019-03-21 11:39:43 -0300

    hello

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"github.com/pkg/errors"
-	"log"
+	"fmt"
 )
 
 func main() {
-	if err := prenda(17); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func prenda(idade int) error {
-	if idade < 18 {
-		return errors.New("prenda: menores de 18 não podem ser presos")
-	}
-	return nil
+	fmt.Println("hello")
 }
